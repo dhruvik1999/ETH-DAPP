5a6c1c84504806c363169e13808777341167729d
dhruvik1999 <navadiyadhruvik2@gmail.com>
2021-02-24 23:38:20 +0530

    setofset cmpl

--- SetOfSet.sol
+++ SetOfSet.sol
@@ -100,6 +100,11 @@ contract SetOfSetToken is ERC20Interface, Owned, SafeMath {
     mapping(address => mapping(address => uint)) allowed;
     
     IERC20 setToken;
+    
+    uint256 ttlSet;
+    uint256[10] units;
+    address[10] sets;
+    mapping(address=>uint256) setUnits;
 
 
     constructor() public {
@@ -108,14 +113,47 @@ contract SetOfSetToken is ERC20Interface, Owned, SafeMath {
         decimals = 18;
         owner = msg.sender;
         _totalSupply = 0;
+        ttlSet=0;
         setToken = IERC20(0x4EcBa77ace8d7Bb94eeDe3A53DA5Eff75b90fC89);
         emit Transfer(address(0), msg.sender, _totalSupply);
     }
+    
+    
+    function addSet(address set, uint256 unit) public{
+        require(setUnits[set]==0,"Set already inserted");
+        require(unit>0,"Units should be >0");
+        require(ttlSet<10,"MAX 10 set can be included");
+        units[ttlSet]=unit;
+        sets[ttlSet]=set;
+        ttlSet+=1;
+        setUnits[set]=unit;
+    }
+    
+    function isSet(address set) public view returns(bool){
+        return !(setUnits[set]>0);
+    }
+    
+    function getUnits(address set) public view returns(uint256){
+        return setUnits[set];
+    }
+    
+    function sendNewToken(address addr, uint256 n) internal{
+        balances[addr] = safeAdd( balances[addr] , n);
+        emit Transfer(0x0000000000000000000000000000000000000000,addr,n);
+    }
+    
+    function issueSetOfSet(uint256 n) public returns(address){
+        for(uint i=0;i<ttlSet;i+=1){
+            // IERC20(sets[i]).approve(this,n*units[i]);
+            IERC20(sets[i]).transferFrom(msg.sender,this, n*units[i]);
+        }
+        _totalSupply+=n;
+        sendNewToken(msg.sender,n);
+    }
 
     function totalSupply() public constant returns (uint) {
-        
-        // return _totalSupply;
-        return setToken.totalSupply();
+        return _totalSupply;
+        // return setToken.totalSupply();
     }
 
 
